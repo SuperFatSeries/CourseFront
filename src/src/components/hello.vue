512ae5a08a6f9ee60d3eafce14805b4048ca3762
Liaowm5 <liaowm5@gmail.com>
2018-10-09 12:42:34 +0800

    add vue router

---
+++ src/src/components/hello.vue
@@ -0,0 +1,12 @@
+<template id="hello">
+  <div>
+    <h1>Hello</h1>
+    <p>This is the tutorial about Hello.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  '/hello': 'Hello'
+}
+</script>
--- src/src/main.js
+++ src/src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './plugins/element.js'
+import Element from 'element-ui';
+import router from './router'
 
 Vue.config.productionTip = false
 
+Vue.use(Element, { size: 'small', zIndex: 3000 });
+
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
